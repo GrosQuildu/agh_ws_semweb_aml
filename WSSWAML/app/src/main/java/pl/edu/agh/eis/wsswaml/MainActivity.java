9dc0d039b835047808fab134758f5b7f4146fcc1
Paweł Płatek <e2.8a.95@gmail.com>
2020-01-17 13:23:07 +0100

    jena works

--- WSSWAML/app/src/main/java/pl/edu/agh/eis/wsswaml/MainActivity.java
+++ WSSWAML/app/src/main/java/pl/edu/agh/eis/wsswaml/MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.StrictMode;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,7 +17,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import wiki.lemedia.sparqlandroid.*;
-import org.apache.jena.query.*;
+import com.hp.hpl.jena.query.*;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,18 +29,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SparqlClient client = new SparqlClient();
+//                SparqlClient client = new SparqlClient();
 
-                String queryString = "select distinct ?Concept where {[] a ?Concept} LIMIT 10";
-                Query query = QueryFactory.create(queryString) ;
+                String wikidataPrefixes = "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
+                        "PREFIX wds: <http://www.wikidata.org/entity/statement/>\n" +
+                        "PREFIX wdv: <http://www.wikidata.org/value/>\n" +
+                        "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+                        "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+                        "PREFIX p: <http://www.wikidata.org/prop/>\n" +
+                        "PREFIX ps: <http://www.wikidata.org/prop/statement/>\n" +
+                        "PREFIX pq: <http://www.wikidata.org/prop/qualifier/>\n" +
+                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                        "PREFIX bd: <http://www.bigdata.com/rdf#>\n\n";
+                String endpointUrl = "https://query.wikidata.org/sparql";
+                String queryString = "SELECT ?item ?itemLabel ?image (MD5(CONCAT(str(?item),str(RAND()))) as ?random)  WHERE {\n" +
+                        "  ?item wdt:P279 wd:Q1778821.\n" +
+                        "  ?item wdt:P18 ?image.\n" +
+                        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\"}\n" +
+                        "} ORDER BY ?random\n" +
+                        "LIMIT 10";
 
-                System.out.println(queryString);
-
-                QueryExecution qexec = QueryExecutionFactory.sparqlService("http://ff-news.ontotext.com/sparql", query);
+                Query query = QueryFactory.create(wikidataPrefixes+queryString) ;
+                QueryExecution qexec = QueryExecutionFactory.sparqlService(endpointUrl, query);
                 ResultSet results = qexec.execSelect();
                 ResultSetFormatter.out(System.out, results, query) ;
             }
