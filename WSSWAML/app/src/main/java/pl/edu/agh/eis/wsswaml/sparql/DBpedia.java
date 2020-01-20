89063d10659b020d205944e8b2d27fe9670cb41a
dos20 <dsepiolo@interia.pl>
2020-01-20 14:25:44 +0100

    Addig DBpedia, inspiration tab

---
+++ WSSWAML/app/src/main/java/pl/edu/agh/eis/wsswaml/sparql/DBpedia.java
@@ -0,0 +1,22 @@
+package pl.edu.agh.eis.wsswaml.sparql;
+
+public class DBpedia extends SimpleEndpointInterface{
+    public String getURL() {
+        return "https://dbpedia.org/sparql";
+    }
+
+    public String getPrefixes() {
+        return  "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+                "PREFIX dc: <http://purl.org/dc/elements/1.1/>\n" +
+                "PREFIX : <http://dbpedia.org/resource/>\n" +
+                "PREFIX dbpedia2: <http://dbpedia.org/property/>\n" +
+                "PREFIX dbpedia: <http://dbpedia.org/>\n" +
+                "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>";
+    }
+
+
+}
