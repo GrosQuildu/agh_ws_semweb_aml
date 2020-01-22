9a5f2bb7abb526cae2761598d52034b5106ef35d
Izzyst <desert.gaara00@gmail.com>
2020-01-22 19:14:33 +0100

    added calling zomato API

---
+++ WSSWAML/app/src/main/java/pl/edu/agh/eis/wsswaml/models/Restaurant.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.eis.wsswaml.models;
+
+public class Restaurant {
+    public String id;
+    public String name;
+    public String url;
+    public String location;
+    public String cuisines;
+    public String timings;
+    public String photoUrl;
+    public String photos;
+}
