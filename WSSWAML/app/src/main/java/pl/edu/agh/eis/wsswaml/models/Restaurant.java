388bad5e00961b25e5a0feb981c6e0ce1cbc83c4
Paweł Płatek <e2.8a.95@gmail.com>
2020-01-26 00:18:39 +0100

    make restaurants parcerable

--- WSSWAML/app/src/main/java/pl/edu/agh/eis/wsswaml/models/Restaurant.java
+++ WSSWAML/app/src/main/java/pl/edu/agh/eis/wsswaml/models/Restaurant.java
@@ -1,10 +1,11 @@
 package pl.edu.agh.eis.wsswaml.models;
 
 import android.location.Location;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-import java.io.Serializable;
 
-public class Restaurant implements Serializable {
+public class Restaurant implements Parcelable {
     public String id;
     public String name;
     public String url;
@@ -14,4 +15,50 @@ public class Restaurant implements Serializable {
     public String photoUrl;
     public String photos;
     public Location location;
+
+    public Restaurant() {
+
+    }
+
+    protected Restaurant(Parcel in) {
+        id = in.readString();
+        name = in.readString();
+        url = in.readString();
+        address = in.readString();
+        cuisines = in.readString();
+        timings = in.readString();
+        photoUrl = in.readString();
+        photos = in.readString();
+        location = in.readParcelable(Location.class.getClassLoader());
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(name);
+        dest.writeString(url);
+        dest.writeString(address);
+        dest.writeString(cuisines);
+        dest.writeString(timings);
+        dest.writeString(photoUrl);
+        dest.writeString(photos);
+        dest.writeParcelable(location, flags);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Restaurant> CREATOR = new Creator<Restaurant>() {
+        @Override
+        public Restaurant createFromParcel(Parcel in) {
+            return new Restaurant(in);
+        }
+
+        @Override
+        public Restaurant[] newArray(int size) {
+            return new Restaurant[size];
+        }
+    };
 }
